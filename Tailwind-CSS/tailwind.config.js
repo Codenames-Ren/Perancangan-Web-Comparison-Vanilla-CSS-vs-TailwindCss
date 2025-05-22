79a399072ceb13f2008e2dc0d5d63cfe90b821b5
Codenames-Ren <renmizuki38@gmail.com>
2025-05-22 13:36:20 +0700

    inisiasi tailwind css untuk perbandingan

---
+++ Tailwind-CSS/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      Poppins: ["Poppins", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
